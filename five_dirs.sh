fa8cbca996590af56c5fccbb531a26099de963c3
pushpit21 <42671080+pushpit21@users.noreply.github.com>
2023-10-20 18:45:41 -0500

    five_dirs and mean updated

--- five_dirs.sh
+++ five_dirs.sh
@@ -1,1 +1,12 @@
-#!/bin/bash 
+#!/bin/bash
+mkdir five
+
+for dir_num in {1..5}; do
+    mkdir "five/dir$dir_num"
+
+    for file_num in {1..4}; do
+        for line in $(seq $file_num); do
+            echo $file_num >> "five/dir$dir_num/file$file_num"
+        done
+    done
+done
--- mean.sh
+++ mean.sh
@@ -1,1 +1,38 @@
 #!/bin/bash
+
+
+if [ "$#" -lt 1 ]; then
+    echo "Usage: $0 <column> [file.csv]" 1>&2
+    exit 1
+fi
+
+column=$1
+shift 
+
+if [ "$#" -ge 1 ]; then
+    file=$1
+else
+    file=/dev/stdin
+fi
+
+# Initialize variables
+sum=0
+line_count=0
+
+# Process the file or stdin
+# tail -n +2 skips the header of the CSV
+tail -n +2 "$file" | cut -d',' -f"$column" | {
+    while read value; do
+        sum=$(echo "$sum + $value" | bc)
+        ((line_count++))
+    done
+
+
+    if [ "$line_count" -eq 0 ]; then
+        echo "Error: No data in column." 1>&2
+        exit 1
+    fi
+
+    mean=$(echo "scale=4; $sum / $line_count" | bc)
+    echo "Mean of column $column: $mean"
+}
