43a98dd5d852854564ff0fabb9ab8c19a632697e
pushpit21 <42671080+pushpit21@users.noreply.github.com>
2023-10-20 18:43:47 -0500

    rm_n and school updated

--- school.sh
+++ school.sh
@@ -1,1 +1,29 @@
 #!/bin/bash
+
+
+sum=0
+
+
+
+count=0
+
+# Process the CSV file
+cat Property_Tax_Roll.csv |
+grep "MADISON SCHOOLS" |
+cut -d',' -f7 |
+{
+    # Read each line (value)
+    while read value; do
+        # Add the value to the sum
+        sum=$(echo "$sum + $value" | bc)
+
+        # Increment the count
+        ((count++))
+    done
+
+    # Calculate the average
+    average=$(echo "scale=2; $sum / $count" | bc)
+
+    # Print the average
+    echo "Average Total Assessed Value for properties in the MADISON SCHOOLS district: $average"
+}
